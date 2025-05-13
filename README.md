514aaeba09e7e484aa1c2d1d66c8b43bd294dd9b
ndt <mail@nathantypanski.com>
2025-05-12 20:58:51 -0400

    refactor some common components into lib/

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # tpm-hacks
 
 Tools & explorations with Linux TPM stack. Scripts I wrote to do things.
+
+## usage
+
+```
+$ RECOVERED=- BLOB_NAME=test ./create-verify-blob
+```
+
+Unsealing
+
+```
+$ ./unseal-blob test -
+```
---
+++ lib/common.sh
@@ -0,0 +1,43 @@
+# common.sh
+#
+# part of tpm-blobstore
+
+TPM_ROOT="${TPM_ROOT:-${HOME}/.tpm/tpm-blobstore}"
+TEMPDIR="$(mktemp -d /tmp/tpm.XXXXXXXXX)"
+
+# extension signaling which primary key is associated with a given secret.
+PRIM_EXTENSION="${PRIM_EXTENSION:-_p384}"
+
+PRIMARY_CTX="${PRIMARY_CTX:-${TPM_ROOT}/prim${PRIM_EXTENSION}.ctx}"
+
+message() {
+    for arg in "${@}"; do
+        if [[ "${arg}" == "" ]]; then
+            printf >&2 "\n"
+        else
+            printf >&2 ">>> %s\n" "${arg}"
+        fi
+    done
+}
+
+ensure_dir() {
+    if [[ $# -ne 1 ]]; then
+        message "ensure_dir called wiith no arguments"
+        return 1
+    fi
+    local dir="${1}"
+    mkdir -p "${TPM_ROOT}"
+}
+
+fail_exists() {
+    if [[ "${FORCE}" = "true" || ${FORCE} = '1' ]]; then
+        return
+    elif [[ -f "${1}" ]]; then
+        message "output filename already exists:" \
+                "    '${1}'" \
+                "exiting. Set FORCE to override."
+        exit 201
+    fi
+}
+
+ensure_dir "${TPM_ROOT}"
